3625f8863e6a5605a9c858f97002ad2e7bc0f75d
ahnilhuman2 <ahnilch1717@gmail.com>
2023-02-01 14:30:33 +0900

    project setting

---
+++ src/test/java/com/example/sekkison/SekkisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sekkison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SekkisonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
