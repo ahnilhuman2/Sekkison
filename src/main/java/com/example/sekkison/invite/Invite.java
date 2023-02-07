6b942a2979e2d20909ee6526b0a18343d2d70a47
ahnilhuman2 <ahnilch1717@gmail.com>
2023-02-07 16:52:59 +0900

    Invite entity setting

--- src/main/java/com/example/sekkison/invite/Invite.java
+++ src/main/java/com/example/sekkison/invite/Invite.java
@@ -1,2 +1,33 @@
-package com.example.sekkison.invite;public class Invite {
+package com.example.sekkison.invite;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "invites")
+@Data
+public class Invite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "from_id")
+    private Long from_id;
+
+    @Column(name = "to_id")
+    private Long to_id;
+
+    @Column(name = "appoint_id")
+    private Long appoint_id;
+
+    @Column(name = "create_at")
+    private LocalDateTime create_at;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updated_at;
+
+
 }
