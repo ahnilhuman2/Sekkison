9d5ef067d8e9c77d45211f8ec418ccc6f7c38312
ahnilhuman2 <ahnilch1717@gmail.com>
2023-02-07 16:39:54 +0900

    Authority entity setting

---
+++ src/main/java/com/example/sekkison/authority/Authority.java
@@ -0,0 +1,18 @@
+package com.example.sekkison.authority;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "authorities")
+@Data
+public class Authority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "auth")
+    private String auth;
+}
