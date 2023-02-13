7595ee5bf91574019fc3b5d0942891c981ebbe13
ahnilhuman2 <ahnilch1717@gmail.com>
2023-02-13 15:44:56 +0900

    UserFileController, UserFileRepository, UserFileService 생성

---
+++ src/main/java/com/example/sekkison/user_file/UserFileController.java
@@ -0,0 +1,11 @@
+package com.example.sekkison.user_file;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userFiles")
+@RequiredArgsConstructor
+public class UserFileController {
+}
---
+++ src/main/java/com/example/sekkison/user_file/UserFileRepository.java
@@ -0,0 +1,6 @@
+package com.example.sekkison.user_file;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserFileRepository extends JpaRepository {
+}
