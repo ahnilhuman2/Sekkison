77fca8d783e1a9494b831b340b5c373311b5f420
ahnilhuman2 <ahnilch1717@gmail.com>
2023-02-13 15:04:56 +0900

    UserController, UserRepository, UserService

---
+++ src/main/java/com/example/sekkison/user/UserController.java
@@ -0,0 +1,14 @@
+package com.example.sekkison.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+
+}
