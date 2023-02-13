1047305b35d7a066123e03ac7e29f476cf061d77
ahnilhuman2 <ahnilch1717@gmail.com>
2023-02-13 15:12:35 +0900

    MyAppointController, MyAppointRepositoy, MyAppointSerivce 생성

---
+++ src/main/java/com/example/sekkison/my_appoint/MyAppointController.java
@@ -0,0 +1,11 @@
+package com.example.sekkison.my_appoint;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/myAppoints")
+@RequiredArgsConstructor
+public class MyAppointController {
+}
---
+++ src/main/java/com/example/sekkison/my_appoint/MyAppointService.java
@@ -0,0 +1,11 @@
+package com.example.sekkison.my_appoint;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MyAppointService {
+}
