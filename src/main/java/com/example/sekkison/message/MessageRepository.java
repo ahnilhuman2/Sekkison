eae9fddc8f7eea3de3fc527d12c3d2f32e045fe1
ahnilhuman2 <ahnilch1717@gmail.com>
2023-02-13 15:36:36 +0900

    MessageController, MessageService, MessageRepository 생성

---
+++ src/main/java/com/example/sekkison/message/MessageRepository.java
@@ -0,0 +1,6 @@
+package com.example.sekkison.message;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepository extends JpaRepository {
+}
---
+++ src/main/java/com/example/sekkison/user_authority/UserAuthorityController.java
@@ -0,0 +1,2 @@
+package com.example.sekkison.user_authority;public class UserAuthorityController {
+}
