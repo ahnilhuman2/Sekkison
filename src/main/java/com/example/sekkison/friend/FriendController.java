ad935131d572feee9a81b9faff6d4bc22a33543e
ahnilhuman2 <ahnilch1717@gmail.com>
2023-02-13 15:29:19 +0900

    FriendController, FriendRepository, FriendService 생성

---
+++ src/main/java/com/example/sekkison/friend/FriendController.java
@@ -0,0 +1,11 @@
+package com.example.sekkison.friend;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/friends")
+@RequiredArgsConstructor
+public class FriendController {
+}
---
+++ src/main/java/com/example/sekkison/friend/FriendRepository.java
@@ -0,0 +1,6 @@
+package com.example.sekkison.friend;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FriendRepository extends JpaRepository {
+}
