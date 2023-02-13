e27cfec38f4e277b5f4923e7600beee91a83b1b1
ahnilhuman2 <ahnilch1717@gmail.com>
2023-02-13 15:16:04 +0900

    AppointController, AppointRepository, AppointService 생성

---
+++ src/main/java/com/example/sekkison/appoint/AppointController.java
@@ -0,0 +1,11 @@
+package com.example.sekkison.appoint;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/myAppoints")
+@RequiredArgsConstructor
+public class AppointController {
+}
---
+++ src/main/java/com/example/sekkison/appoint/AppointRepository.java
@@ -0,0 +1,6 @@
+package com.example.sekkison.appoint;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppointRepository extends JpaRepository {
+}
