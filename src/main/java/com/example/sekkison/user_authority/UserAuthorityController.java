15949dba908625a5c5820124c5ebe9cc96e61c38
ahnilhuman2 <ahnilch1717@gmail.com>
2023-02-13 15:40:31 +0900

    UserAuthorityController, UserAuthorityService, UserAuthorityRepsitory 생성

--- src/main/java/com/example/sekkison/user_authority/UserAuthorityController.java
+++ src/main/java/com/example/sekkison/user_authority/UserAuthorityController.java
@@ -1,2 +1,11 @@
-package com.example.sekkison.user_authority;public class UserAuthorityController {
+package com.example.sekkison.user_authority;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userAuthorities")
+@RequiredArgsConstructor
+public class UserAuthorityController {
 }
---
+++ src/main/java/com/example/sekkison/user_authority/UserAuthorityRepository.java
@@ -0,0 +1,6 @@
+package com.example.sekkison.user_authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAuthorityRepository extends JpaRepository {
+}
