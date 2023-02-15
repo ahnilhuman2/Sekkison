564168fbe2f9c15e8a35fb1767d1c411738bc95d
ahnilhuman2 <ahnilch1717@gmail.com>
2023-02-15 17:47:20 +0900

    user update

--- src/main/java/com/example/sekkison/user/UserService.java
+++ src/main/java/com/example/sekkison/user/UserService.java
@@ -15,24 +15,19 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public User findByUsername(String username) {
-        return userRepository.findByUsername(username);
-    }
-
-    public boolean isExist(String username) {
-        User user = userRepository.findByUsername(username);
-        if (user != null) return true;
-        return false;
-    }
-
     public ErrorForm register(User user) {
         String username = user.getUsername().toUpperCase(Locale.ROOT);
         String password = user.getPassword();
         String name = user.getName();
         String phone = user.getPhone();
-        char gender = user.getGender();
+        Character gender = user.getGender();
 
-        Pattern pattern = Pattern.compile("^[0-9]*$");
+        if (username == null) return new ErrorForm("아이디를 입력해주세요", false);
+        if (password == null) return new ErrorForm("비밀번호를 입력해주세요", false);
+        if (name == null) return new ErrorForm("별명을 입력해주세요", false);
+        if (phone == null) return new ErrorForm("전화번호를 입력해주세요", false);
+        if (gender == null) return new ErrorForm("성별을 입력해주세요", false);
+
 
 
         if (username.length() < 4 || username.length() >10) {
@@ -45,13 +40,13 @@ public class UserService {
             return errorId;
         }
         // 비밀번호 영어 및 숫자를 허용하며, 숫자키와 관련된 특수문자만 허용한다
-        if (!Pattern.matches("^[a-zA-Z\\d`~!@#$%^&*()-_=+]{8,16}$", username)) {
+        if (!Pattern.matches("^[a-zA-Z\\d`~!@#$%^&*()-_=+]{8,16}$", password)) {
             ErrorForm errorPw = new ErrorForm("비밀번호는 8자 이상 16자 이하여야 합니다", false);
             return errorPw;
         }
 
-        if (!Pattern.matches("^[가-힣]{2,4}$", name)) {
-            ErrorForm errorName = new ErrorForm("이름은 한글표기, 2-4자여야 합니다", false);
+        if (!Pattern.matches("^[가-힣a-zA-Z]{2,10}$", name)) {
+            ErrorForm errorName = new ErrorForm("이름은 한글, 영어표기, 2-10자여야 합니다", false);
             return errorName;
         }
 
@@ -60,7 +55,7 @@ public class UserService {
             return errorPhone;
         }
 
-        if (gender != 'M' || gender != 'F') {
+        if (gender != 'M' && gender != 'F') {
             ErrorForm errorGender = new ErrorForm("성별은 M이나 F로 입력해주세요", false);
             return errorGender;
         }
