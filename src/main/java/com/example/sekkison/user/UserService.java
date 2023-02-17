0ca8947d7dcaf7bd75bcff297ee558d996c2c566
ahnilhuman2 <ahnilch1717@gmail.com>
2023-02-17 16:47:52 +0900

    UserService update

--- src/main/java/com/example/sekkison/user/UserService.java
+++ src/main/java/com/example/sekkison/user/UserService.java
@@ -30,41 +30,42 @@ public class UserService {
 
         Pattern pattern = Pattern.compile("^[0-9]*$");
 
+        ResponseForm responseForm = new ResponseForm();
 
         if (username.length() < 4 || username.length() >10) {
-            ResponseForm errorId = new ResponseForm("아이디는 4자 이상 10자 이하여야 합니다", false);
-            return errorId;
+            responseForm.setError("아이디는 4자 이상 10자 이하여야 합니다", false);
+            return responseForm;
         }
 
         if (!Pattern.matches("^[a-zA-Z0-9]*$", username)) {
-            ResponseForm errorId = new ResponseForm("아이디는 영문자와 숫자만 포함되어야 합니다", false);
-            return errorId;
+            responseForm.setError("아이디는 영문자와 숫자만 포함되어야 합니다", false);
+            return responseForm;
         }
         // 비밀번호 영어 및 숫자를 허용하며, 숫자키와 관련된 특수문자만 허용한다
         if (!Pattern.matches("^[a-zA-Z\\d`~!@#$%^&*()-_=+]{8,16}$", username)) {
-            ResponseForm errorPw = new ResponseForm("비밀번호는 8자 이상 16자 이하여야 합니다", false);
-            return errorPw;
+            responseForm.setError("비밀번호는 8자 이상 16자 이하여야 합니다", false);
+            return responseForm;
         }
 
         if (!Pattern.matches("^[가-힣]{2,4}$", name)) {
-            ResponseForm errorName = new ResponseForm("이름은 한글표기, 2-4자여야 합니다", false);
-            return errorName;
+            responseForm.setError("이름은 한글표기, 2-4자여야 합니다", false);
+            return responseForm;
         }
 
         if (!Pattern.matches("^01([0|1|6|7|8|9])?([0-9]{3,4})?([0-9]{4})$", phone)) {
-            ResponseForm errorPhone = new ResponseForm("전화번호는 - 제외 11자리로 입력해주세요", false);
-            return errorPhone;
+            responseForm.setError("전화번호는 - 제외 11자리로 입력해주세요", false);
+            return responseForm;
         }
 
         if (gender != 'M' || gender != 'F') {
-            ResponseForm errorGender = new ResponseForm("성별은 M이나 F로 입력해주세요", false);
-            return errorGender;
+            responseForm.setError("성별은 M이나 F로 입력해주세요", false);
+            return responseForm;
         }
 
         user = userRepository.save(user);
-        ResponseForm error = new ResponseForm("", true);
+        responseForm.setSuccess(true, null);
 
-        return error;
+        return responseForm;
 
 
     }
