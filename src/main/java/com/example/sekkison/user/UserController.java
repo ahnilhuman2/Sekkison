e0668f95ebb5cdbdde66c2a79e00b97b8a6e557c
ahnilhuman2 <ahnilch1717@gmail.com>
2023-02-21 15:21:08 +0900

    update, 코드 주석 추가

--- src/main/java/com/example/sekkison/user/UserController.java
+++ src/main/java/com/example/sekkison/user/UserController.java
@@ -16,6 +16,7 @@ public class UserController {
 
     @ResponseBody
     @PostMapping("")
+    // 회원가입
     public ResponseForm join(@RequestBody @Validated User user) {
 
         return userService.register(user);
@@ -23,40 +24,53 @@ public class UserController {
 
     @ResponseBody
     @PostMapping("/login")
+    // 로그인
     public ResponseForm login(@Validated User user, HttpSession session) {
 
         return userService.login(user, session);
     }
 
+    @ResponseBody
     @GetMapping("")
+    // id로 user객체 받기
     public ResponseForm returnUser(Long userId) {
 
         return userService.getUser(userId);
     }
 
+    @ResponseBody
     @PutMapping("/{userId}")
+    // 회원정보수정
     public ResponseForm updateUser(@Validated @PathVariable("userId") Long userId, User user) {
         return userService.updateUser(userId, user);
     }
 
+    @ResponseBody
     @DeleteMapping("")
+    // 회원탈퇴
     public ResponseForm deleteUser(@Validated User user, Long userId) {
         return userService.deleteUser(user, userId);
     }
 
+    @ResponseBody
     @GetMapping("/duplicated/{parameter}")
+    // 아이디, 별명, 전화번호 중복체크
     public ResponseForm duplicateUser(@Validated @PathVariable("parameter") Integer parameter, String str) {
         return userService.duplicate(str, parameter);
     }
 
+    @ResponseBody
     @GetMapping("/my_list/{userId}/{parameter}")
+    // 친구 초대 리스트, 약속 초대 리스트
     public ResponseForm myList(
             @PathVariable("userId") Long userId,
             @PathVariable("parameter") Integer parameter) {
         return userService.myList(userId, parameter);
     }
 
+    @ResponseBody
     @GetMapping("/search")
+    // 유저 검색
     public ResponseForm search(String str, Long userId) {
         return userService.searchUser(str, userId);
     }
