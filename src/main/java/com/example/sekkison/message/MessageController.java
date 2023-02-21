1aa134a1827f77884d88f0a4dbb34b85ef2dbee7
ahnilhuman2 <ahnilch1717@gmail.com>
2023-02-21 15:57:35 +0900

    MessageController, MessagRepository, MessageService  Message List 생성

--- src/main/java/com/example/sekkison/message/MessageController.java
+++ src/main/java/com/example/sekkison/message/MessageController.java
@@ -1,11 +1,23 @@
 package com.example.sekkison.message;
 
+import com.example.sekkison.common.ResponseForm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/messages")
 @RequiredArgsConstructor
 public class MessageController {
+
+    private final MessageService messageService;
+
+    @ResponseBody
+    @GetMapping("/list")
+    public ResponseForm list(Long userId) {
+        return messageService.messageList(userId);
+    }
+
 }
--- src/main/java/com/example/sekkison/message/MessageService.java
+++ src/main/java/com/example/sekkison/message/MessageService.java
@@ -1,11 +1,25 @@
 package com.example.sekkison.message;
 
+import com.example.sekkison.common.ResponseForm;
+import com.example.sekkison.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class MessageService {
+
+    private final UserRepository userRepository;
+    private final MessageRepository messageRepository;
+
+    public ResponseForm messageList(Long userId) {
+        ResponseForm responseForm = new ResponseForm();
+        List<Message> messages = messageRepository.findByToId(userId);
+
+        return responseForm.setSuccess(true, messages);
+    }
 }
