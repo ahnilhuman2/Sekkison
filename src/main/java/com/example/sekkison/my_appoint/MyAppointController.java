7c7989f29e0f4593e0b85df142b2812586c392dd
ahnilhuman2 <ahnilch1717@gmail.com>
2023-02-22 16:05:17 +0900

    MyAppointController, MyApponitService  isMaster() 생성

--- src/main/java/com/example/sekkison/my_appoint/MyAppointController.java
+++ src/main/java/com/example/sekkison/my_appoint/MyAppointController.java
@@ -1,11 +1,22 @@
 package com.example.sekkison.my_appoint;
 
+import com.example.sekkison.common.ResponseForm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/myAppoints")
 @RequiredArgsConstructor
 public class MyAppointController {
+
+    private final MyAppointService myAppointService;
+
+    @ResponseBody
+    @GetMapping("/is_master")
+    public ResponseForm isMaster(Long userId, Long appointId) {
+        return myAppointService.isMaster(userId, appointId);
+    }
 }
