a41c15ab8d419432925d7b786aaaab78c371a982
ahnilhuman2 <ahnilch1717@gmail.com>
2023-02-22 13:48:32 +0900

    denyFriend, sendFriend 생성

--- src/main/java/com/example/sekkison/friend/FriendController.java
+++ src/main/java/com/example/sekkison/friend/FriendController.java
@@ -1,11 +1,26 @@
 package com.example.sekkison.friend;
 
+import com.example.sekkison.common.ResponseForm;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/friends")
 @RequiredArgsConstructor
 public class FriendController {
+
+    private final FriendService friendService;
+
+    @ResponseBody
+    @DeleteMapping("")
+    public ResponseForm deny(Long friendId) {
+        return friendService.deny(friendId);
+    }
+
+    @ResponseBody
+    @PostMapping("")
+    public ResponseForm send(Friend friend) {
+        return friendService.send(friend);
+    }
+
 }
--- src/main/java/com/example/sekkison/friend/FriendService.java
+++ src/main/java/com/example/sekkison/friend/FriendService.java
@@ -1,5 +1,6 @@
 package com.example.sekkison.friend;
 
+import com.example.sekkison.common.ResponseForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +9,25 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class FriendService {
+
+    private final FriendRepository friendRepository;
+
+    public ResponseForm deny(Long friendId) {
+        ResponseForm responseForm = new ResponseForm();
+        Friend deleteFriend = friendRepository.findById(friendId).orElse(null);
+
+        if (deleteFriend == null) {
+            return responseForm.setError("존재하지 않는 친구입니다", false);
+        }
+
+        friendRepository.delete(deleteFriend);
+        return responseForm.setSuccess(true, null);
+    }
+
+    public ResponseForm send(Friend friend) {
+        ResponseForm responseForm = new ResponseForm();
+        friend.setIsAccepted(false);
+        friendRepository.save(friend);
+        return responseForm.setSuccess(true, null);
+    }
 }
