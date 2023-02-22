5a3884ef1cea940a495f6c9446acedd429e4e880
ahnilhuman2 <ahnilch1717@gmail.com>
2023-02-22 15:51:03 +0900

    InviteController, InviteService deny 생성

--- src/main/java/com/example/sekkison/invite/InviteController.java
+++ src/main/java/com/example/sekkison/invite/InviteController.java
@@ -1,11 +1,22 @@
 package com.example.sekkison.invite;
 
+import com.example.sekkison.common.ResponseForm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("invites")
 @RequiredArgsConstructor
 public class InviteController {
+
+    private final InviteService inviteService;
+
+    @ResponseBody
+    @DeleteMapping("")
+    public ResponseForm deny(Long inviteId) {
+        return inviteService.deny(inviteId);
+    }
 }
